66c47a46d4d05ec7dc4651748976e13d0f221d53
TekfolioNG <tekfoilong@gmail.com>
2025-06-02 21:01:20 +0100

    switched to pnpm build

--- plugins/paystack.client.ts
+++
@@ -1,6 +0,0 @@
-
-export default defineNuxtPlugin((nuxtApp) => {
-const paystackScript = document.createElement('script')
-paystackScript.src = 'https://js.paystack.co/v1/inline.js'
-document.head.appendChild(paystackScript)
-})
